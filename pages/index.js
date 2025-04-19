e369470f48b317b7ccab5409c271b3325c312784
Alexandre Rayol <alexandrerayol.dev@gmail.com>
2025-04-19 20:54:21 -0300

    adiciona pagina index.js

---
+++ pages/index.js
@@ -0,0 +1,40 @@
+import { useState } from "react"
+
+function HeroComponent() {
+    const [isDarkMode, setIsDarkMode] = useState(true)
+
+    function OnHandleClick() {
+        setIsDarkMode(!isDarkMode);
+    }
+
+    const mainElementStyles = {
+        borderRadius: '4px',
+        height: '400px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: isDarkMode ? '#726E75' : '#FFFFFF',
+        color: isDarkMode ? 'white' : 'black',
+        flexDirection: 'column',
+        gap: '1rem',
+        fontFamily: 'sans-serif'
+    }
+
+    const buttonElementStyle =
+    {
+        cursor: 'pointer'
+    }
+
+    return (
+        <>
+            <main style={mainElementStyles}>
+                <span>Alexandre Rayol</span>
+                <button onClick={OnHandleClick} style={buttonElementStyle}>Alterar tema </button>
+            </main>
+        </>
+    )
+}
+
+export default function Home() {
+    return HeroComponent()
+}
