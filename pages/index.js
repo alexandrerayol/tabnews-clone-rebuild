9aa24b1b81fd9699c5b339001d3db567376f339b
Alexandre Rayol <alexandrerayol.dev@gmail.com>
2025-07-20 19:56:43 -0300

    add `prettier` como dependencia de desenvolvimento e scripts `lint:check e lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,41 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function HeroComponent() {
-    const [isDarkMode, setIsDarkMode] = useState(true)
+  const [isDarkMode, setIsDarkMode] = useState(true);
 
-    function OnHandleClick() {
-        setIsDarkMode(!isDarkMode);
-    }
+  function OnHandleClick() {
+    setIsDarkMode(!isDarkMode);
+  }
 
-    const mainElementStyles = {
-        borderRadius: '4px',
-        height: '400px',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: isDarkMode ? '#56666B' : '#EEE1B3',
-        color: isDarkMode ? 'white' : 'black',
-        flexDirection: 'column',
-        gap: '1rem',
-        fontFamily: 'sans-serif'
-    }
+  const mainElementStyles = {
+    borderRadius: "4px",
+    height: "400px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: isDarkMode ? "#56666B" : "#EEE1B3",
+    color: isDarkMode ? "white" : "black",
+    flexDirection: "column",
+    gap: "1rem",
+    fontFamily: "sans-serif",
+  };
 
-    const buttonElementStyle =
-    {
-        cursor: 'pointer'
-    }
+  const buttonElementStyle = {
+    cursor: "pointer",
+  };
 
-    return (
-        <>
-            <main style={mainElementStyles}>
-                <span>Alexandre Rayol</span>
-                <button onClick={OnHandleClick} style={buttonElementStyle}>Alterar tema </button>
-            </main>
-        </>
-    )
+  return (
+    <>
+      <main style={mainElementStyles}>
+        <span>Alexandre Rayol</span>
+        <button onClick={OnHandleClick} style={buttonElementStyle}>
+          Alterar tema{" "}
+        </button>
+      </main>
+    </>
+  );
 }
 
 export default function Home() {
-    return HeroComponent()
+  return HeroComponent();
 }
