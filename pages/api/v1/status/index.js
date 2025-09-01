a2721ecec865b77ff74231a58c126223b467f9eb
Alexandre Rayol <alexandrerayol.dev@gmail.com>
2025-09-01 16:57:54 -0300

    finish `/api/v1/status` enpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,13 +1,32 @@
 import database from "infra/database.js";
 
-export default async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as SUM"); //try connect to postgres
+export default async function status(request, response) {
+  const updatedAt = new Date(Date.now()).toISOString();
 
-  console.log(result.rows[0]);
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
 
-  res.status(200).json({
-    status: 200,
-    success: true,
-    message: "endpoint /v1/status adicionado com sucesso",
+  const dbMaxConnectionsResult = await database.query("SHOW max_connections");
+  const dbMaxConnectionsValue = parseInt(
+    dbMaxConnectionsResult.rows[0].max_connections,
+  );
+
+  const dbName = process.env.POSTGRES_DB;
+
+  const dbOpenedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+  const dbOpenedConnectionsValue = dbOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: dbMaxConnectionsValue,
+        opened_connections: dbOpenedConnectionsValue,
+        version: dbVersionValue,
+      },
+    },
   });
 }
