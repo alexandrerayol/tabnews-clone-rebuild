a7ac6cfdd3d8cefbd8bc2317729d743fafe0f5c1
Alexandre Rayol <alexandrerayol.dev@gmail.com>
2025-09-01 16:56:06 -0300

    add error handling to `database.js`

--- infra/database.js
+++ infra/database.js
@@ -1,25 +1,24 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const config = {
+  const client = new Client({
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
-  };
-
-  console.log(config);
-
-  const client = new Client(config);
+  });
 
   await client.connect();
 
-  const result = await client.query(queryObject);
-
-  await client.end();
-
-  return result;
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
