44fc0c954f274f4a7bae2bcf4821a36c8eec8f90
Alexandre Rayol <alexandrerayol.dev@gmail.com>
2025-09-02 09:27:29 -0300

    move `client.connect()` into error handling

--- infra/database.js
+++ infra/database.js
@@ -1,21 +1,25 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
+  const clientConfig = {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
-  });
+  };
 
-  await client.connect();
+  const client = new Client(clientConfig);
+
+  console.log(`Credenciais do banco de dados: `, clientConfig);
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
